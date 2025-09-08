6369eb99808c3086faf060c0ae304251e4cf7af2
Richard-m-j <richardj815165@gmail.com>
2025-09-08 17:12:34 +0530

    fix: relative path

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04f59c565deeb2199"
   instance_type = "t2.large"
   key_name      = "richardnv"
-  user_data     = file("${path.root}/setup.sh")
+  user_data     = file("../setup.sh")
   tags = {
     Name = "Richard_Instance"
   }
