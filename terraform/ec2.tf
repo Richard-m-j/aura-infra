ca388182b0d849120aef9b54f66bfdb70de654c3
Richard-m-j <richardj815165@gmail.com>
2025-09-08 17:29:40 +0530

    feat: increase the time for workflow

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0a84ffe13366e143f"
   instance_type = "t2.large"
   key_name      = "richardnv"
-  user_data     = file("../setup.sh")
+  user_data     = file("${path.module}/setup.sh")
   tags = {
     Name = "Richard_Instance"
   }
