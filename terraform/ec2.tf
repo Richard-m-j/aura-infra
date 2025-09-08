ac2420c39dc93590471700fe539543124deb926b
Richard-m-j <richardj815165@gmail.com>
2025-09-08 17:04:38 +0530

    feat: terraform

---
+++ terraform/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "web" {
+  ami           = "ami-04f59c565deeb2199"
+  instance_type = "t2.medium"
+  key_name      = "richardnv"
+  user_data     = file("${path.root}/setup.sh")
+  tags = {
+    Name = "Richard_Instance"
+  }
+}
