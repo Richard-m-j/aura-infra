12683c053a71cf3e7d8b59d69d4f5cb983035778
Richard-m-j <richardj815165@gmail.com>
2025-09-08 17:09:09 +0530

    chore: made large ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-04f59c565deeb2199"
-  instance_type = "t2.medium"
+  instance_type = "t2.large"
   key_name      = "richardnv"
   user_data     = file("${path.root}/setup.sh")
   tags = {
