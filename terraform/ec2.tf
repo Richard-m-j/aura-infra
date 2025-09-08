ddc846a1f781ffc08392c7df05bbaadef2565789
Richard-m-j <richardj815165@gmail.com>
2025-09-08 18:25:16 +0530

    feat: setup file template

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0a84ffe13366e143f"
   instance_type = "t2.large"
   key_name      = "richardnv"
-  user_data     = file("${path.module}/setup.sh")
+  user_data = templatefile("${path.module}/setup.tftpl", {})
   tags = {
     Name = "Richard_Instance"
   }
