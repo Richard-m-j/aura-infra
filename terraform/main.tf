0a3598c086bcff60fd5576d1690aacef3a950d91
Richard-m-j <richardj815165@gmail.com>
2025-09-10 15:41:56 +0530

    feat: s3 and dynamo db for terraform lock

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "aura-app-richard" 
+    key            = "terraform.tfstate"       
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks-richard"   
+    encrypt        = true
+  }
 }
  
 provider "aws" {
