bf9015889caa8d0eb6cca52332cc1b593972391a
Richard-m-j <richardj815165@gmail.com>
2025-09-13 09:49:58 +0530

    feat: spot instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,13 @@ resource "aws_instance" "web" {
   ami           = var.instance_configuration.ami
   instance_type = var.instance_configuration.instance_type
   key_name      = var.instance_configuration.key_name
-
+  
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "terminate"
+    }
+  }
   user_data = templatefile("${path.module}/setup.sh", {
     argocd_password = var.argocd_password
     grafana_password = var.grafana_password
